1e2a1663a28f9992ff2324717b5389a9ba87496b
wildlifehexagon <hartline.eric@gmail.com>
2021-03-15 10:00:31 -0500

    refactor: move graphql helper fns to utils

--- src/CatalogContainer.test.tsx
+++ src/CatalogContainer.test.tsx
@@ -1,10 +1,7 @@
 import { MockedProvider } from "@apollo/client/testing"
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import CatalogContainer, {
-  getQueryFilterString,
-  normalizeDataObject,
-} from "./CatalogContainer"
+import CatalogContainer from "./CatalogContainer"
 import { CatalogProvider } from "./CatalogContext"
 import {
   GET_BACTERIAL_STRAIN_LIST,
@@ -224,63 +221,5 @@ describe("CatalogContainer", () => {
       const firstAvailRow = await screen.findByText(mockAvailableItem)
       expect(firstAvailRow).toBeInTheDocument()
     })
-  })
-})
-
-describe("getQueryFilterString function", () => {
-  it("should remove strain_type", () => {
-    expect(getQueryFilterString(["strain_type: all"])).toEqual("")
-  })
-  it("should remove in_stock", () => {
-    expect(getQueryFilterString(["in_stock: true"])).toEqual("")
-  })
-  it("should remove both strain_type and in_stock", () => {
-    expect(
-      getQueryFilterString(["in_stock: true", "strain_type: all"]),
-    ).toEqual("")
-  })
-  it("should include AND operator", () => {
-    expect(
-      getQueryFilterString(["strain_type: all", "label: sad", "summary: test"]),
-    ).toEqual("label~=sad;summary~=test")
-  })
-})
-
-describe("normalizeDataObject function", () => {
-  it("should return listStrains", () => {
-    const strainObj = {
-      listStrains: "abc",
-    }
-    expect(normalizeDataObject(strainObj)).toEqual("abc")
-  })
-  it("should return listRegularStrains", () => {
-    const strainObj = {
-      listRegularStrains: "reg",
-    }
-    expect(normalizeDataObject(strainObj)).toEqual("reg")
-  })
-  it("should return listGWDIStrains", () => {
-    const strainObj = {
-      listGWDIStrains: "gwdi",
-    }
-    expect(normalizeDataObject(strainObj)).toEqual("gwdi")
-  })
-  it("should return listStrainsInventory", () => {
-    const strainObj = {
-      listStrainsInventory: "inv",
-    }
-    expect(normalizeDataObject(strainObj)).toEqual("inv")
-  })
-  it("should return listBacterialStrains", () => {
-    const strainObj = {
-      listBacterialStrains: "bac",
-    }
-    expect(normalizeDataObject(strainObj)).toEqual("bac")
-  })
-  it("should return same data for unexpected object", () => {
-    const strainObj = {
-      foo: "bar",
-    }
-    expect(normalizeDataObject(strainObj)).toStrictEqual(strainObj)
   })
 })
