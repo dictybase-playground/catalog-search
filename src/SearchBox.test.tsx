ef93839c1615c8265708e4d582de71e6831ca860
wildlifehexagon <hartline.eric@gmail.com>
2021-03-22 13:43:29 -0500

    test: add tests for autocomplete options display

--- src/SearchBox.test.tsx
+++ src/SearchBox.test.tsx
@@ -2,7 +2,11 @@ import React from "react"
 import { screen, render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import SearchBox from "./SearchBox"
-import { CatalogContext, catalogReducer } from "./CatalogContext"
+import {
+  CatalogProvider,
+  CatalogContext,
+  catalogReducer,
+} from "./CatalogContext"
 
 describe("SearchBox", () => {
   const MockComponent = ({ activeFilters }: { activeFilters: string[] }) => {
@@ -39,5 +43,23 @@ describe("SearchBox", () => {
     userEvent.click(clear)
     // should only show the clear button
     expect(screen.getAllByRole("button")).toHaveLength(1)
+  })
+
+  it("should not show any items in dropdown if user is entering value", () => {
+    render(
+      <CatalogProvider>
+        <SearchBox />
+      </CatalogProvider>,
+    )
+    // type into searchbox
+    userEvent.type(screen.getByRole("textbox"), "Desc")
+    // find and click the descriptor option
+    const descriptorOption = screen.getByRole("option", { name: "Descriptor" })
+    expect(descriptorOption).toBeInTheDocument()
+    userEvent.click(descriptorOption)
+    // descriptor option should not appear after typing this time
+    userEvent.type(screen.getByRole("textbox"), "Desc")
+    expect(descriptorOption).not.toBeInTheDocument()
+    expect(screen.getByText(/Descriptor:/)).toBeInTheDocument()
   })
 })
