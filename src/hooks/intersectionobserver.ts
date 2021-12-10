09f666b4c937ac06d8dbef4aedeac63a92f2d1a7
Siddhartha Basu <biosidd@gmail.com>
2021-12-09 19:26:44 -0600

    feat: add an intersection observer hook with callback action

---
+++ src/hooks/intersectionobserver.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react"
+import {AppProps} from "../types/props"
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+const useIntersectionObserver = (props: AppProps.UseIntersectionObserverProps): void => {
+  const { target, option, onIntersection } = props
+  const observerRef = useRef<IntersectionObserver>()
+
+  // set up the intersection observer
+  // callback fn that adds intersection observer to observer ref
+  // and observes the target ref if it exists
+  // https://medium.com/@teh_builder/ref-objects-inside-useeffect-hooks-eb7c15198780#
+  useEffect(() => {
+    if (!target.current) return
+    if (observerRef.current) observerRef.current.disconnect()
+    observerRef.current = new IntersectionObserver(onIntersection, option)
+    observerRef.current.observe(target.current)
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect()
+      }
+    }
+  }, [option, target, onIntersection])
+}
+
+export default useIntersectionObserver
