9497cb0b060aaeccb961f0e3e55fdd3d55b61eb7
Siddhartha Basu <biosidd@gmail.com>
2021-12-13 15:00:06 -0600

    feat: use 50% of parent window size for holding the scrolling table

--- src/components/CatalogListWrapper.tsx
+++ src/components/CatalogListWrapper.tsx
@@ -1,32 +1,24 @@
 import { AppProps } from "../types/props"
 import { makeStyles } from "@material-ui/core/styles"
+import { DefaultTheme } from "@material-ui/styles";
 import Paper from "@material-ui/core/Paper"
 import List from "@material-ui/core/List"
+import useWindowSize from "../hooks/useWindowSize"
 
 
-const useStyles = makeStyles(() => ({
-  container: {
-    height: "310px",
+const useStyles = makeStyles<DefaultTheme, AppProps.SizeProps>({
+  root: {
+    height: ({ height }) => height,
     overflow: "auto",
-  },
-  row: {
-    borderBottom: "1px solid rgba(224, 224, 224, 1)",
-    "&:hover": {
-      backgroundColor: "#eeeeee",
-      boxShadow:
-        "inset 1px 0 0 #dadce0,inset -1px 0 0 #dadce0,0 1px 2px 0 rgba(60,64,67,.3),0 1px 3px 1px rgba(60,64,67,.15)",
-      zIndex: 1,
-    },
-  },
-  loading: {
-    color: "tomato",
-  },
-}))
+  }
+})
 
 export default function CatalogListWrapper({ root: rootRef, children }: AppProps.CatalogListWrapperProps): JSX.Element {
-  const classes = useStyles()
+  const {height} = useWindowSize()
+  // 50% of actual client window height
+  const classes = useStyles({ height: height && 50*height/100 })
   return (
-    <Paper ref={rootRef} className={classes.container}>
+    <Paper ref={rootRef} className={classes.root}>
       <List>
         {children}
       </List>
