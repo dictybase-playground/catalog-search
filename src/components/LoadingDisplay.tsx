a23136b08ca96f362ed117dc0b06d6ad940b2581
Siddhartha Basu <biosidd@gmail.com>
2021-12-14 11:44:53 -0600

    refactor: repeat skeletons and set up a matching height

--- src/components/LoadingDisplay.tsx
+++ src/components/LoadingDisplay.tsx
@@ -1,3 +1,12 @@
+import { Skeleton } from "@material-ui/lab";
+import { Box } from "@material-ui/core";
+
 export default function LoadingDisplay(): JSX.Element {
-  return <h2>....Loading data....</h2>
+  return (
+    <Box mx={8} mt={4}>
+      {[...Array(10)].map((_, i: number) => {
+        return <Skeleton key={i} height={35} />
+      })}
+    </Box>
+  )
 }
