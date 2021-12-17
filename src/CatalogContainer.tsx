20bf3b218eac9e7be49271af40d6b04d50ee517c
Siddhartha Basu <biosidd@gmail.com>
2021-12-17 12:25:14 -0600

    refactor: use dataField for retrieving data based on search query

--- src/CatalogContainer.tsx
+++ src/CatalogContainer.tsx
@@ -1,49 +1,65 @@
 // import SearchBox from "./SearchBox"
 // import FilterDropdown from "./FilterDropdown"
-import Box from "@material-ui/core/Box"
-import LoadingDisplay from "./components/LoadingDisplay.bs"
-import ErrorDisplay from "./components/ErrorDisplay"
-import { StrainListDocument } from "dicty-graphql-schema";
-import useIntersectionObserver from "./hooks/intersectionobserver"
-import CatalogListWrapper from "./components/CatalogListWrapper"
-import CatalogTableDisplay from "./components/CatalogTableDisplay"
+import { Box } from "@material-ui/core"
 import { useRef } from "react"
+import { useSearchParams } from "react-router-dom"
 import { useQuery } from "@apollo/client"
 
-const CatalogContainer = () => {
-  const rootRef = useRef<HTMLDivElement>(null)
-  const targetRef = useRef<HTMLTableRowElement>(null)
-  const { loading, error, data, fetchMore } = useQuery(
-    StrainListDocument, {
-    variables: { cursor: 0, limit: 12, filter: "" }
-  })
-  const onIntersection = ([entry]: IntersectionObserverEntry[]) => {
-    const { nextCursor } = data.listStrains
-    if (!entry.isIntersecting) return
-    if (!nextCursor) return
-    fetchMore({ variables: { cursor: nextCursor } })
-  }
-  useIntersectionObserver({
-    target: targetRef,
-    option: { root: rootRef.current, threshold: 0.10 },
-    onIntersection: onIntersection
-  })
+import LoadingDisplay from "./components/LoadingDisplay.bs"
+import ErrorDisplay from "./components/ErrorDisplay"
+import CatalogListWrapper from "./components/CatalogListWrapper"
+import CatalogTableDisplay from "./components/CatalogTableDisplay"
+import useIntersectionObserver from "./hooks/intersectionobserver"
+import useStrainCatalogSearch from "./hooks/useStrainCatalogSearch"
+import { graphqlQueryVars, strainConfig } from "./config"
 
-  return (
-    <>
-      {/* <Box m={2} display="flex" flexDirection="row">
-        <FilterDropdown />
-        <SearchBox />
-      </Box> */}
-      <Box>
-        <CatalogListWrapper root={rootRef}>
-          {loading && <LoadingDisplay />}
-          {error && <ErrorDisplay error={error} />}
-          {data && <CatalogTableDisplay data={data} target={targetRef} />}
-        </CatalogListWrapper>
-      </Box>
-    </>
-  )
+
+
+export default function CatalogContainer() {
+    const [searchParams] = useSearchParams()
+    const { query, dataField, filter } = useStrainCatalogSearch({
+        searchParams,
+        strainConfig,
+        value: "regular"
+    })
+
+    const gqvars = { variables: { filter: filter, ...graphqlQueryVars } }
+    const { loading, error, data, fetchMore } = useQuery(query, gqvars)
+
+    const rootRef = useRef<HTMLDivElement>(null)
+    const targetRef = useRef<HTMLTableRowElement>(null)
+    const onIntersection = ([entry]: IntersectionObserverEntry[]) => {
+        if (!entry.isIntersecting) return
+        const { nextCursor } = data?.[dataField]
+        if (!nextCursor) return
+        fetchMore({ variables: { cursor: nextCursor } })
+    }
+    useIntersectionObserver({
+        target: targetRef,
+        onIntersection: onIntersection,
+        option: { root: rootRef.current, threshold: 0.10 }
+    })
+
+    return (
+        <>
+            {/* <Box m={2} display="flex" flexDirection="row"> */}
+            {/* <FilterDropdown /> */}
+            {/* <SearchBox /> */}
+            {/*  </Box>*/}
+            <Box>
+                <CatalogListWrapper root={rootRef}>
+                    {loading ? <LoadingDisplay /> : <></>}
+                    {error ? <ErrorDisplay error={error} /> : <></>}
+                    {data?.[dataField]
+                        ? <CatalogTableDisplay
+                            data={data}
+                            dataField={dataField}
+                            target={targetRef}
+                        />
+                        : <></>
+                    }
+                </CatalogListWrapper>
+            </Box>
+        </>
+    )
 }
-
-export default CatalogContainer
